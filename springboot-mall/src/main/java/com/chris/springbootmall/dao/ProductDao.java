467f6ab0489c7d88cdfa5bc6164b7ae6c7c5416c
chriz-chen <yfchen1217@gmail.com>
2024-04-09 23:15:54 +0800

    「商品功能 - 實作查詢商品列表（基礎實作）」

--- springboot-mall/src/main/java/com/chris/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/chris/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.chris.springbootmall.dao;
 import com.chris.springbootmall.dto.ProductRequest;
 import com.chris.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/chris/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/chris/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.chris.springbootmall.service;
 import com.chris.springbootmall.dto.ProductRequest;
 import com.chris.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
