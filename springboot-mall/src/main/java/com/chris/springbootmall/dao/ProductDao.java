97d59c93296931e7a87e3057da7b28b4d6b6a422
chriz-chen <yfchen1217@gmail.com>
2024-04-09 11:25:09 +0800

    「商品功能 - 實作新增商品」

--- springboot-mall/src/main/java/com/chris/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/chris/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.chris.springbootmall.dao;
 
+import com.chris.springbootmall.dto.ProductRequest;
 import com.chris.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/chris/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/chris/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.chris.springbootmall.service;
 
+import com.chris.springbootmall.dto.ProductRequest;
 import com.chris.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
