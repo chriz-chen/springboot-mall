5c8ac6d7576f27c992f4d557b0563f0ef8d62cbc
chriz-chen <yfchen1217@gmail.com>
2024-04-10 14:26:43 +0800

    商品功能 - 實作查詢商品列表（分頁 + 總筆數）

--- springboot-mall/src/main/java/com/chris/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/chris/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.chris.springbootmall.dto.ProductQueryParams;
 import com.chris.springbootmall.dto.ProductRequest;
 import com.chris.springbootmall.model.Product;
 import com.chris.springbootmall.service.ProductService;
+import com.chris.springbootmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,7 @@ public class ProductController {
 
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -46,9 +47,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
